fc4e3658ce6af22d6d3b893d305b469b7b20dc12
fjchen7 <fjchen7@gmail.com>
2025-01-22 16:24:12 +0800

    Add method get_raydium_pool_position

--- src/balance_fetcher.rs
+++ src/balance_fetcher.rs
@@ -1,5 +1,11 @@
 use std::str::FromStr;
+use anchor_lang::AccountDeserialize;
+use raydium_amm_v3::libraries::{get_delta_amount_0_unsigned, get_delta_amount_1_unsigned, tick_math};
+use solana_account_decoder::parse_token::{TokenAccountType, UiAccountState};
+use solana_account_decoder::UiAccountData;
 use solana_client::rpc_client::RpcClient;
+use solana_rpc_client_api::request::TokenAccountsFilter;
+use solana_sdk::account::Account;
 use solana_sdk::pubkey::Pubkey;
 
 type Result<T> = anyhow::Result<T>;
@@ -76,6 +82,142 @@ impl BalanceFetcher {
         let spl_token = SPLToken { amount, decimals };
         Ok(spl_token)
     }
+
+    /// Fetch LP position amounts of Raydium pool
+    ///
+    /// # Arguments
+    /// - `wallet_address` - The wallet address
+    /// - `pool_id` - The pool ID, e.g. 8sLbNZoA1cfnvMJLPfp98ZLAnFSYCFApfJKMbiXNLwxj (SOL-USDC.1bp Pool in Raydium mainnet)
+    /// - `raydium_v3_program` - The Raydium V3 program ID, e.g. CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK (Raydium mainnet program ID)
+    ///
+    /// # Returns
+    /// - `(u64, u64)` - The total amount of token 0 and token 1 in wallet_address's LP position in the pool
+    pub fn raydium_pool_position(&self, wallet_address: &Pubkey, pool_id: &Pubkey, raydium_v3_program: &Pubkey) -> Result<(u64, u64)> {
+        let positions = self.get_nft_account_and_position_by_owner(
+            &wallet_address,
+            spl_token_2022::id(),
+            &raydium_v3_program,
+        );
+        let positions: Vec<Pubkey> = positions
+            .iter()
+            .map(|item| item.position)
+            .collect();
+        let positions = self.rpc.get_multiple_accounts(&positions)?;
+
+        let positions = positions.into_iter().filter_map(|p|
+            match p {
+                None => None,
+                Some(rsp) => {
+                    let position = deserialize_anchor_account::<
+                        raydium_amm_v3::states::PersonalPositionState,
+                    >(&rsp);
+                    match position {
+                        Err(_) => {
+                            log::warn!("deserialize_anchor_account error");
+                            None
+                        }
+                        Ok(position) => {
+                            if position.pool_id == *pool_id {
+                                Some(position)
+                            } else {
+                                None
+                            }
+                        }
+                    }
+                }
+            }
+        ).collect::<Vec<_>>();
+        let mut amount_0 = 0;
+        let mut amount_1 = 0;
+        for position in positions {
+            let tick_lower_price_x64 = tick_math::get_sqrt_price_at_tick(position.tick_lower_index)?;
+            let tick_upper_price_x64 = tick_math::get_sqrt_price_at_tick(position.tick_upper_index)?;
+            let delta_amount0 =
+                get_delta_amount_0_unsigned(tick_lower_price_x64, tick_upper_price_x64, position.liquidity, true)?;
+            let delta_amount1 =
+                get_delta_amount_1_unsigned(tick_upper_price_x64, tick_lower_price_x64, position.liquidity, true)?;
+            amount_0 += delta_amount0;
+            amount_1 += delta_amount1;
+        };
+        Ok((amount_0, amount_1))
+    }
+
+    // Reference: https://github.com/raydium-io/raydium-clmm/blob/master/client/src/main.rs#L281
+    fn get_nft_account_and_position_by_owner(
+        &self,
+        owner: &Pubkey,
+        token_program: Pubkey,
+        raydium_amm_v3_program: &Pubkey,
+    ) -> Vec<PositionNftTokenInfo> {
+        let all_tokens = self.rpc
+            .get_token_accounts_by_owner(owner, TokenAccountsFilter::ProgramId(token_program))
+            .unwrap();
+        let mut position_nft_accounts = Vec::new();
+        for keyed_account in all_tokens {
+            if let UiAccountData::Json(parsed_account) = keyed_account.account.data {
+                if parsed_account.program == "spl-token" || parsed_account.program == "spl-token-2022" {
+                    if let Ok(TokenAccountType::Account(ui_token_account)) =
+                        serde_json::from_value(parsed_account.parsed)
+                    {
+                        let _frozen = ui_token_account.state == UiAccountState::Frozen;
+
+                        let token = ui_token_account
+                            .mint
+                            .parse::<Pubkey>()
+                            .unwrap_or_else(|err| panic!("Invalid mint: {}", err));
+                        let token_account = keyed_account
+                            .pubkey
+                            .parse::<Pubkey>()
+                            .unwrap_or_else(|err| panic!("Invalid token account: {}", err));
+                        let token_amount = ui_token_account
+                            .token_amount
+                            .amount
+                            .parse::<u64>()
+                            .unwrap_or_else(|err| panic!("Invalid token amount: {}", err));
+
+                        let _close_authority = ui_token_account.close_authority.map_or(*owner, |s| {
+                            s.parse::<Pubkey>()
+                                .unwrap_or_else(|err| panic!("Invalid close authority: {}", err))
+                        });
+
+                        if ui_token_account.token_amount.decimals == 0 && token_amount == 1 {
+                            let (position_pda, _) = Pubkey::find_program_address(
+                                &[
+                                    raydium_amm_v3::states::POSITION_SEED.as_bytes(),
+                                    token.to_bytes().as_ref(),
+                                ],
+                                &raydium_amm_v3_program,
+                            );
+                            position_nft_accounts.push(PositionNftTokenInfo {
+                                key: token_account,
+                                program: token_program,
+                                position: position_pda,
+                                mint: token,
+                                amount: token_amount,
+                                decimals: ui_token_account.token_amount.decimals,
+                            });
+                        }
+                    }
+                }
+            }
+        }
+        position_nft_accounts
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct PositionNftTokenInfo {
+    key: Pubkey,
+    program: Pubkey,
+    position: Pubkey,
+    mint: Pubkey,
+    amount: u64,
+    decimals: u8,
+}
+
+pub fn deserialize_anchor_account<T: AccountDeserialize>(account: &Account) -> Result<T> {
+    let mut data: &[u8] = &account.data;
+    T::try_deserialize(&mut data).map_err(Into::into)
 }
 
 #[cfg(test)]
@@ -109,5 +251,18 @@ mod tests {
         let balance_spl_token = balancer_fetcher.balance_spl_token(&wallet, &token_mint_address).unwrap();
         assert!(balance_spl_token.amount > 0);
         assert_eq!(balance_spl_token.decimals, 9);
+    }
+
+    #[test]
+    fn test_get_raydium_pool_position() {
+        let fetcher = new_balancer_fetcher();
+        let wallet = Pubkey::from_str("53zSj4G935ZY2a5x2UnGAiJXSuXXmGHaLph2zhAUvYpg").unwrap();
+        let raydium_v3_program = Pubkey::from_str("CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK").unwrap();
+        // SOL-USDC.1bp Pool
+        let pool_id = Pubkey::from_str("8sLbNZoA1cfnvMJLPfp98ZLAnFSYCFApfJKMbiXNLwxj").unwrap();
+        let (amount_0, amount_1) = fetcher.raydium_pool_position(&wallet, &pool_id, &raydium_v3_program).unwrap();
+        println!("amount_0: {}, amount_1: {}", amount_0, amount_1);
+        assert!(amount_0 > 0);
+        assert!(amount_1 > 0);
     }
 }
